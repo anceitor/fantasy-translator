06adc47cc95243b57ca6db424a5eaa353b95e3fa
anceito <anceitor@gmail.com>
2020-09-14 15:25:20 -0500

    Updates

---
+++ precache-manifest.d44ac5516591198e95aa4c94dbb57baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edfda1a7f34a435f6d224dd503a2946",
+    "url": "/fantasy-translator/index.html"
+  },
+  {
+    "revision": "28ffb69157cd1bc84c05",
+    "url": "/fantasy-translator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "844da017dbdbc6bc808d",
+    "url": "/fantasy-translator/static/js/2.4aed5b96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fantasy-translator/static/js/2.4aed5b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ffb69157cd1bc84c05",
+    "url": "/fantasy-translator/static/js/main.4ec80feb.chunk.js"
+  },
+  {
+    "revision": "07f90e816873e46f665b",
+    "url": "/fantasy-translator/static/js/runtime-main.b44d2862.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fantasy-translator/static/media/logo.5d5d9eef.svg"
+  }
+]);
